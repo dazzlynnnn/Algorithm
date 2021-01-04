d720a209dd96cbff63305c74d33b97df76112529
Lynn <dazzlynnnn@gmail.com>
2021-01-04 20:38:41 +0900

    백준 1008 - A/B

---
+++ BOJ/1008/src/Main.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args){
+        Scanner s = new Scanner(System.in);
+        double a = s.nextDouble();
+        double b = s.nextDouble();
+        System.out.println(a/b);
+    }
+}
