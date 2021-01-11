77219635d4bbe02109cd69fcdbd5ab6325c0380f
Lynn <dazzlynnnn@gmail.com>
2021-01-11 12:47:38 +0900

    백준 2420 - 사파리월드

---
+++ BOJ/2420/src/Main.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class Main {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        long n = s.nextLong();
+        long m = s.nextLong();
+
+        System.out.println(Math.abs(n-m));
+    }
+}
