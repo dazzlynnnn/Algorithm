bd393621cd9735327e0fd2ba2cd9679381044287
Lynn <dazzlynnnn@gmail.com>
2021-01-22 11:50:41 +0900

    백준 1152 - 단어의 개수

---
+++ BOJ/1152/src/Main.java
@@ -0,0 +1,16 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        bw.write(Integer.toString(st.countTokens()));
+
+        bw.flush();
+        br.close();
+        bw.close();
+    }
+}
