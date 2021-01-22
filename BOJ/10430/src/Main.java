fc6f7a1d442005ef2a177649456ddf9de20a9381
Lynn <dazzlynnnn@gmail.com>
2021-01-22 12:16:30 +0900

    백준 10430 - 나머지

---
+++ BOJ/10430/src/Main.java
@@ -0,0 +1,20 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int a = Integer.parseInt(st.nextToken());
+        int b = Integer.parseInt(st.nextToken());
+        int c = Integer.parseInt(st.nextToken());
+
+        System.out.println((a+b)%c);
+        System.out.println(((a%c)+(b+c))%c);
+        System.out.println((a*b)%c);
+        System.out.println(((a%c)*(b%c))%c);
+
+        br.close();
+    }
+}
