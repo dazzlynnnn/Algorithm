36382e1c67ecdd4ee33afb5e628f5866f7909f69
Lynn <dazzlynnnn@gmail.com>
2021-01-31 18:12:44 +0900

    백준 2741 - N 찍기

---
+++ BOJ/2741/src/Main.java
@@ -0,0 +1,18 @@
+import java.io.*;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int n = Integer.parseInt(br.readLine());
+        for (int i=1; i<=n; i++){
+            bw.write(Integer.toString(i)+"\n");
+        }
+        bw.flush();
+        br.close();
+        bw.close();
+    }
+}
