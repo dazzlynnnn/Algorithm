b5dd7709a0f797675f1a5c91450a9db664d158be
Lynn <dazzlynnnn@gmail.com>
2021-01-31 18:10:29 +0900

    백준 15552 - 빠른 A+B

---
+++ BOJ/15552/src/Main.java
@@ -0,0 +1,23 @@
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st;
+
+        int t = Integer.parseInt(br.readLine());
+        for (int i=0; i<t; i++){
+            st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            bw.write(Integer.toString(a+b)+"\n");
+        }
+        bw.flush();
+        br.close();
+        bw.close();
+    }
+}
