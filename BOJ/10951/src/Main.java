23c0fd7335d5df9e7b9182d4240ebe6267086ba4
Lynn <dazzlynnnn@gmail.com>
2021-01-31 20:09:01 +0900

    백준 10951 - A+B - 4

---
+++ BOJ/10951/src/Main.java
@@ -0,0 +1,10 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        Scanner sc = new Scanner(System.in);
+        while(sc.hasNext()){
+            System.out.println(sc.nextInt()+sc.nextInt());
+        }
+    }
+}
