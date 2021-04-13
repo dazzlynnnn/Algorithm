dfed802c7a4897bd15a76d10de96b9701cf58fa6
Lynn <dazzlynnnn@gmail.com>
2021-04-13 20:07:02 +0900

    프로그래머스 level 1 - K번째 수

--- Programmers/K번째수/src/Main.java
+++ Programmers/K번째수/src/Main.java
@@ -7,17 +7,18 @@ public class Main {
 	    int[] array = {1,5,2,6,3,7,4};
 	    int[][] commands = {{2,5,3},{4,4,1},{1,7,3}};
 	    System.out.println(s.solution(array, commands)[0]);
+        System.out.println(s.solution(array, commands)[1]);
+        System.out.println(s.solution(array, commands)[2]);
     }
 }
-
 
 class Solution {
     public int[] solution(int[] array, int[][] commands) {
         int[] answer = new int[commands.length];
         for (int i=0; i< commands.length; i++){
-            int[] temp = new int[array.length];
-            for (int j=commands[i][0]-1; j<commands[i][1]-1; j++){
-                temp[i] = array[j];
+            int[] temp = new int[commands[i][1]-commands[i][0]+1];
+            for (int j=0; j<temp.length; j++){
+                temp[j] = array[j+commands[i][0]-1];
             }
             Arrays.sort(temp);
             answer[i] = temp[commands[i][2]-1];
